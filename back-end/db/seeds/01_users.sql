4c131cdd77e22ecb115a33b337321ac9d943f98f
Sean <seanhoyeonjin@gmail.com>
2022-07-03 21:32:56 -0400

    database is now seeded

--- back-end/db/seeds/01_users.sql
+++ back-end/db/seeds/01_users.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (first_name, last_name, email, password)
-VALUES ("Sean", "Jin", "seanhoyeonjin@gmail.com", "password")
+VALUES ('Sean', 'Jin', 'seanhoyeonjin@gmail.com', 'password')
--- back-end/db/seeds/05_comments.sql
+++ back-end/db/seeds/05_comments.sql
@@ -1,3 +1,3 @@
 INSERT INTO comments (resource_id, comment)
-VALUES (1, "this video is cool!"),
-(2, "this video is great!");
+VALUES (1, 'this video is cool!'),
+(2, 'this video is great!');
