f6f27b83d855bd5667569ccee7c566c0c9740020
Sean <seanhoyeonjin@gmail.com>
2022-07-22 15:01:13 -0400

    heroku changes

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -1,13 +1,20 @@
 const pg = require("pg");
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
+// const client = new pg.Client({
+//   connectionString: connectionString || process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
 const client = new pg.Client({
-  connectionString: connectionString || process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
 });
 
 console.log(`Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}`);
