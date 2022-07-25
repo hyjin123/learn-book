d608eb4150de13700039440add18d5eb6f748013
Sean <seanhoyeonjin@gmail.com>
2022-07-25 16:46:51 -0400

    added access control allow origin

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axios1 = axios.create({
   baseURL: "https://hoyeonjin-learnbook.herokuapp.com",
   timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
+  headers: { "Access-Control-Allow-Origin": "*" },
 });
 
 export default axios1;
