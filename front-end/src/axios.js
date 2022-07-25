984afa2fe111fa4e453a420b5878bbc386088731
Sean <seanhoyeonjin@gmail.com>
2022-07-25 16:45:20 -0400

    created new instance of axios to see if this works

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axios1 = axios.create({
-  baseURL: "https://hoyeonjin-learnbook.herokuapp.com/",
+  baseURL: "https://hoyeonjin-learnbook.herokuapp.com",
   timeout: 1000,
   headers: { "X-Custom-Header": "foobar" },
 });
