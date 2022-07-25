6a8fa32326980ee4cd516d3a59b79bf75edecdde
Sean <seanhoyeonjin@gmail.com>
2022-07-25 14:49:26 -0400

    some changes to http proxy middleware

--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -2,13 +2,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const express = require("express");
 
 const app = express();
+// place in src with index.js no need to import anywhere
+const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  // add other server routes to path array
   app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://hoyeonjin-learnbook.herokuapp.com",
-      changeOrigin: true,
-    })
+    proxy(["/api"], { target: "https://hoyeonjin-learnbook.herokuapp.com" })
   );
 };
