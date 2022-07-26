f45db00396d5aa7caeb9f01f461ded2c214c5b8a
Sean <seanhoyeonjin@gmail.com>
2022-07-26 11:53:06 -0400

    using axios instance to try solving it again

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const axios1 = axios.create({
   baseURL: "https://hoyeonjin-learnbook.herokuapp.com/",
   headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
   },
 });
 
