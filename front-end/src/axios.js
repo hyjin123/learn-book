c7bf8ba2380dc03f41e1a18e6e9a02f84e0b80bd
Sean <seanhoyeonjin@gmail.com>
2022-07-26 18:34:03 -0400

    new solution

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -2,12 +2,6 @@ import axios from "axios";
 
 const axios1 = axios.create({
   baseURL: "https://hoyeonjin-learnbook.herokuapp.com/",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Credentials": true,
-  },
 });
 
 export default axios1;
