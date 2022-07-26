c70b8952c801d33190e2b728953bd199de7492c0
Sean <seanhoyeonjin@gmail.com>
2022-07-26 15:46:25 -0400

    new solution

--- back-end/app.js
+++ back-end/app.js
@@ -14,10 +14,9 @@ const app = express();
 // );
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://hoyeonjin-learnbook.herokuapp.com/"
   );
   next();
 });
