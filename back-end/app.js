4e45d638338b1bfe166223a03c85d21d1f765af3
Sean <seanhoyeonjin@gmail.com>
2022-07-26 15:37:28 -0400

    new solution

--- back-end/app.js
+++ back-end/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
