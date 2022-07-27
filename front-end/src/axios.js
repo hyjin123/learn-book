88a72607abb81d4b33a94888a4e2f3c8c6797b99
Sean <seanhoyeonjin@gmail.com>
2022-07-27 17:32:40 -0400

    changed content type to application/json

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axios1 = axios.create({
   baseURL: "https://hoyeonjin-learnbook.herokuapp.com/",
   headers: {
+    "Content-Type": "application/json",
     "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Credentials": true,
     "Access-Control-Allow-Origin": "*",
