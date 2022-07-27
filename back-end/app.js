4868673e94e83d0c9562b7eb3f495184fbb9746e
Sean <seanhoyeonjin@gmail.com>
2022-07-27 11:28:00 -0400

    added proxy middleware

--- back-end/app.js
+++ back-end/app.js
@@ -83,9 +83,6 @@ const app = express();
 // };
 
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const express = require("express");
-
-const app = express();
 
 module.exports = function (app) {
   app.use(
