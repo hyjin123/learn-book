ad3dbdf8a6a777c17e837c2043dc769d9c72f50d
Sean <seanhoyeonjin@gmail.com>
2022-07-27 17:28:44 -0400

    changes

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const axios1 = axios.create({
   baseURL: "https://hoyeonjin-learnbook.herokuapp.com/",
   headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
     "Access-Control-Allow-Headers": "*",
     "Access-Control-Allow-Credentials": true,
     "Access-Control-Allow-Origin": "*",
