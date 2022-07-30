ec6a831ddc01bb9eed3f84084658913a40c58619
Sean <seanhoyeonjin@gmail.com>
2022-07-30 15:36:16 -0400

    changed settings for production

--- back-end/db/index.js
+++ back-end/db/index.js
@@ -6,17 +6,17 @@ const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 
 // used in development
-const client = new pg.Client({
-  connectionString: connectionString || process.env.DATABASE_URL,
-});
-
-// used in production
 // const client = new pg.Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   connectionString: connectionString || process.env.DATABASE_URL,
 // });
+
+used in production;
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 console.log(`Connected to ${process.env.DB_NAME} on ${process.env.DB_HOST}`);
 client.connect();
--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 // used in production
-// const axios1 = axios.create({
-//   baseURL: "https://hoyeonjin-learnbook.herokuapp.com/",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "Access-Control-Allow-Headers": "*",
-//     "Access-Control-Allow-Credentials": true,
-//     "Access-Control-Allow-Origin": "*",
-//     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-//   },
-// });
+const axios1 = axios.create({
+  baseURL: "https://hoyeonjin-learnbook.herokuapp.com/",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+});
 
 // used in development
-const axios1 = axios.create({
-  baseURL: "http://localhost:3002",
-});
+// const axios1 = axios.create({
+//   baseURL: "http://localhost:3002",
+// });
 
 export default axios1;
